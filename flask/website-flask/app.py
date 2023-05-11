8c00738fc71fd59c3dc4ae507ab261f696d8d600
xzh <shadow1028@aliyun.com>
2023-05-11 16:31:47 +0800

    update

--- flask/website-flask/app.py
+++ flask/website-flask/app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api")
+def hello_api():
+    return "<p>Hello, Api!</p>"
+
+if __name__ == '__main__':
+    app.run()
